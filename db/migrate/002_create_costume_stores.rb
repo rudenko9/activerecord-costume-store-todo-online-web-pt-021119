b12469f67f57efeb68f8737755b5a9a5cbc3a5c1
Nataliya R <n.rudenko@gmail.com>
2019-05-08 02:38:42 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,6 +6,7 @@ class CreateCostumeStore < ActiveRecord::Migration
       t.string :name
       t.string :location
       t.integer :number of costumes, or "costume inventory"
+      
       t.string :hometown
     end
   end
