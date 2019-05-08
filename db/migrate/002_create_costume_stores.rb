d4b4f780beb46cef38e96ec4470e020b321cbefa
Nataliya R <n.rudenko@gmail.com>
2019-05-08 03:07:48 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,7 +5,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      
+      t.integer :costume_inventory
       t.integer :number_of_employees
       t.string :whether_or_not_its_still_in_business
       t.datetime :opening_time
