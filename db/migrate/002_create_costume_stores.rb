1837db436ae16c09a3eb32f693a2329d575a3e00
Nataliya R <n.rudenko@gmail.com>
2019-05-08 02:40:47 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,8 +5,8 @@ class CreateCostumeStore < ActiveRecord::Migration
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      t.integer :number of costumes, or "costume inventory"
-      t.integer :number of employees
+      t.integer :number_of_costumes, or "costume inventory"
+      t.integer :number_of_employees
       t.string :whether or not it's still in business'
       t.integer :opening_time
       t.integer :closing_time
