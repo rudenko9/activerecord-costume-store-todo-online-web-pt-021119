36c2705e0242c48b4502a8bb307d2b6fed541d48
Nataliya R <n.rudenko@gmail.com>
2019-05-08 03:14:51 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -9,7 +9,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.integer :number_of_employees
       t.boolean :still_in_business
       t.datetime :opening_time
-       t.datetime :closing_time
+      t.datetime :closing_time
     end
   end
   
