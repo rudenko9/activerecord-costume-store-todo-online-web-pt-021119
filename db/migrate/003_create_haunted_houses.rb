318ba8f4be525c4aec24d7a7737b4eec26c0326d
Nataliya R <n.rudenko@gmail.com>
2019-05-08 02:48:33 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,4 +2,11 @@
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change
     create_table :haunted_houses do |t|
-      
+    t.string  :name
+location
+theme
+price
+whether they're family friendly or not
+opening date
+closing date
+long description
