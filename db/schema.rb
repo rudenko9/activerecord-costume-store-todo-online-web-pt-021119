db62cdf7a9799dffb30728525c8b1f2bcacdf1bf
Nataliya R <n.rudenko@gmail.com>
2019-05-08 03:12:26 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "location"
     t.string "theme"
     t.float "price"
-    t.boolean "family_friendly_or_not"
+    t.boolean "family_friendly"
     t.datetime "opening_date"
     t.datetime "closing_date"
     t.text "description"
