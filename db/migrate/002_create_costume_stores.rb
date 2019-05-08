f22c7738e71ba966aeb660bf3a8bcf9630af751c
Nataliya R <n.rudenko@gmail.com>
2019-05-08 02:37:18 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,7 +1,14 @@
 # Create your costume_stores migration here
 
 class CreateCostumeStore < ActiveRecord::Migration
-  
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
   
   
 end 
