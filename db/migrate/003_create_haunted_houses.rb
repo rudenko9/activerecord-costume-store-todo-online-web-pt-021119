824792c425aa08948c26facce1ba7036e60d076f
Nataliya R <n.rudenko@gmail.com>
2019-05-08 03:13:23 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,7 +6,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
      t.string :location
      t.string :theme
      t.float :price
-     t.boolean :family_friendly
+     t.boolean :whethe_they_are_family_friendly_or_not
      t.datetime :opening_date
      t.datetime :closing_date
      t.text :description
